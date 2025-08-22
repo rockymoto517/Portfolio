4fa8dcb6ffceddfcc2a5aea1778ca1cc0b6bdd2f
rockymoto517 <rockymoto517@gmail.com>
2025-08-22 17:16:18 -0500

    Use hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./components/theme-provider";
 import "./index.css";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createRouter,
+  createHashHistory,
+} from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import App from "./App.tsx";
+
+// Memory history
+const hashHistory = createHashHistory();
 
 // Set up a Router instance
 const router = createRouter({
   routeTree,
   defaultPreload: "intent",
   scrollRestoration: true,
+  history: hashHistory,
 });
 
 // Register things for typesafety
